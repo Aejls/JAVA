063c02fc972cc4cfba43537b3a4fc8f6da7f1a52
Sejal Chauhan <86917180+Aejls@users.noreply.github.com>
2025-02-06 13:09:41 +0530

    Add files via upload

---
+++ Expression.java
@@ -0,0 +1,7 @@
+//Calculate value of this expression  7/4 * 9/2
+public class Expression {
+    public static void main(String[] args) {
+        float a = 7/4.0f * 9/2.0f;
+        System.out.println(a);
+    }
+}
---
+++ WriteExpression.java
@@ -0,0 +1,12 @@
+//Write the following expression in a java program:
+//(v^2-u^2)/2as
+public class WriteExpression {
+    public static void main(String[] args) {
+        float v=40;
+        float u=4;
+        float a=6;
+        float s=8;
+        float formula=(v*v - u*u) /(2*a*s);
+        System.out.println(formula);
+    }
+}
